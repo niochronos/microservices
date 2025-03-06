b595aa6bc99641e333a71413b2a1e0c9b5a8c9ef
Yaroslav Oliinyk <niochronos@gmail.com>
2025-03-06 18:00:27 +0200

    Loans Microservice: Init

---
+++ loans/src/test/java/com/oliinyk/yaroslav/loans/LoansApplicationTests.java
@@ -0,0 +1,13 @@
+package com.oliinyk.yaroslav.loans;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LoansApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
