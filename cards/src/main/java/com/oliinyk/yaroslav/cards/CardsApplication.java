976ca49583bf8e2ce7e4710d5f06c0b1d7cd6ee0
Yaroslav Oliinyk <niochronos@gmail.com>
2025-03-10 16:16:14 +0200

    Cards Microservice: Implementation

--- cards/src/main/java/com/oliinyk/yaroslav/cards/CardsApplication.java
+++ cards/src/main/java/com/oliinyk/yaroslav/cards/CardsApplication.java
@@ -1,9 +1,39 @@
 package com.oliinyk.yaroslav.cards;
 
+import io.swagger.v3.oas.annotations.ExternalDocumentation;
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Contact;
+import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.annotations.info.License;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
+/*@ComponentScans({ @ComponentScan("com.oliinyk.yaroslav.cards.controller") })
+@EnableJpaRepositories("com.oliinyk.yaroslav.cards.repository")
+@EntityScan("com.oliinyk.yaroslav.cards.model")*/
+@EnableJpaAuditing(auditorAwareRef = "auditAwareImpl")
+@OpenAPIDefinition(
+		info = @Info(
+				title = "Cards microservice REST API Documentation",
+				description = "Bank Cards microservice REST API Documentation",
+				version = "v1",
+				contact = @Contact(
+						name = "Madan Reddy",
+						email = "MyEmail@mail.com",
+						url = "https://www.some-site.com"
+				),
+				license = @License(
+						name = "Apache 2.0",
+						url = "https://www.some-site.com"
+				)
+		),
+		externalDocs = @ExternalDocumentation(
+				description = "Bank Cards microservice REST API Documentation",
+				url = "https://www.some-site.com/swagger-ui.html"
+		)
+)
 public class CardsApplication {
 
 	public static void main(String[] args) {
