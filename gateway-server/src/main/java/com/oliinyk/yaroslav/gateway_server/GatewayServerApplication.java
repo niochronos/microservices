8272b37c3093ab9afcdb4debae87c8c71eb829aa
Yaroslav Oliinyk <niochronos@gmail.com>
2025-04-03 20:37:16 +0300

    Gateway Server:  - Implemented Circuit Breaker Pattern

--- gateway-server/src/main/java/com/oliinyk/yaroslav/gateway_server/GatewayServerApplication.java
+++ gateway-server/src/main/java/com/oliinyk/yaroslav/gateway_server/GatewayServerApplication.java
@@ -21,19 +21,25 @@ public class GatewayServerApplication {
 				.route(p -> p
 						.path("/bank/accounts/**")
 						.filters(f -> f.rewritePath("/bank/accounts/(?<segment>.*)", "/${segment}")
-								.addResponseHeader("X-Response-Time", LocalDateTime.now().toString()))
+								.addResponseHeader("X-Response-Time", LocalDateTime.now().toString())
+								.circuitBreaker(config -> config.setName("accountsCircuitBreaker")
+										.setFallbackUri("forward:/contactSupport")
+								)
+						)
 						.uri("lb://ACCOUNTS")
 				)
 				.route(p -> p
 						.path("/bank/loans/**")
 						.filters(f -> f.rewritePath("/bank/loans/(?<segment>.*)", "/${segment}")
-								.addResponseHeader("X-Response-Time", LocalDateTime.now().toString()))
+								.addResponseHeader("X-Response-Time", LocalDateTime.now().toString())
+						)
 						.uri("lb://LOANS")
 				)
 				.route(p -> p
 						.path("/bank/cards/**")
 						.filters(f -> f.rewritePath("/bank/cards/(?<segment>.*)", "/${segment}")
-								.addResponseHeader("X-Response-Time", LocalDateTime.now().toString()))
+								.addResponseHeader("X-Response-Time", LocalDateTime.now().toString())
+						)
 						.uri("lb://CARDS")
 				)
 				.build();
