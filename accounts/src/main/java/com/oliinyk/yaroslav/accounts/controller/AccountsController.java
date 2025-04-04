7aa831ffeeace8c9420a6268b6d71bb0cf842851
Yaroslav Oliinyk <niochronos@gmail.com>
2025-04-04 18:33:40 +0300

    Accounts Microservice:  - Implemented Retry Pattern

--- accounts/src/main/java/com/oliinyk/yaroslav/accounts/controller/AccountsController.java
+++ accounts/src/main/java/com/oliinyk/yaroslav/accounts/controller/AccountsController.java
@@ -6,6 +6,7 @@ import com.oliinyk.yaroslav.accounts.dto.CustomerDto;
 import com.oliinyk.yaroslav.accounts.dto.ErrorResponseDto;
 import com.oliinyk.yaroslav.accounts.dto.ResponseDto;
 import com.oliinyk.yaroslav.accounts.service.IAccountsService;
+import io.github.resilience4j.retry.annotation.Retry;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.media.Content;
 import io.swagger.v3.oas.annotations.media.Schema;
@@ -14,6 +15,8 @@ import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import jakarta.validation.constraints.Pattern;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.core.env.Environment;
@@ -23,6 +26,8 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.concurrent.TimeoutException;
+
 @Tag(
         name = "CRUD REST APIs for Accounts Bank",
         description = "CRUD REST API for Bank to CREATE, UPDATE, FETCH, DELETE account details"
@@ -31,6 +36,8 @@ import org.springframework.web.bind.annotation.*;
 @RequestMapping(path = "/api", produces = {MediaType.APPLICATION_JSON_VALUE})
 @Validated
 public class AccountsController {
+
+    private static final Logger logger = LoggerFactory.getLogger(AccountsController.class);
 
     private final IAccountsService accountsService;
 
@@ -166,11 +173,19 @@ public class AccountsController {
                     description = "HTTP Status Internal Server Error"
             )
     })
+    @Retry(name = "getBuildInfo", fallbackMethod = "getBuildInfoFallback")
     @GetMapping("/build-info")
-    public ResponseEntity<String> getBuildInfo() {
+    public ResponseEntity<String> getBuildInfo() throws TimeoutException {
+        logger.debug("Invoked method getBuildInfo()");
         return ResponseEntity
                 .status(HttpStatus.OK)
                 .body("build.version: " + buildVersion);
+    }
+    public ResponseEntity<String> getBuildInfoFallback(Throwable throwable) {
+        logger.debug("Invoked method getBuildInfoFallback()");
+        return ResponseEntity
+                .status(HttpStatus.OK)
+                .body("build.version: " + buildVersion + " (from Fallback)");
     }
 
     @Operation(
