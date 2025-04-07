755a26b03203ad35a267933731fa35f4d3ba4985
Yaroslav Oliinyk <niochronos@gmail.com>
2025-04-07 22:56:08 +0300

    Microservices:  - Added script to push docker images

---
+++ docker-push-images.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+# Array of images
+declare -a images=(
+    "accounts"
+    "loans"
+    "cards"
+    "config-server"
+    "eureka-server"
+    "gateway-server"
+)
+
+declare tag=":s11"
+
+# Loop through each images and run the commands
+for image in "${images[@]}"; do
+    echo --------------------------------------------------
+    echo "Docker image push docker.io/niochronos/$image$tag"
+    docker image push docker.io/niochronos/"$image$tag" || { echo "Docker push failed $image"; exit 1; }
+    echo "Successfully pushed $image"
+done
+
+echo "All images pushed successfully!"
