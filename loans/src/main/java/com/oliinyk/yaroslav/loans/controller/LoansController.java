3a63c3a4716fc10e1270e81c606135ef4c6fecf6
Yaroslav Oliinyk <niochronos@gmail.com>
2025-04-08 15:50:13 +0300

    Microservices:  - Implemented Tracing using Grafana, Tempo and OpenTelemetry

--- loans/src/main/java/com/oliinyk/yaroslav/loans/controller/LoansController.java
+++ loans/src/main/java/com/oliinyk/yaroslav/loans/controller/LoansController.java
@@ -99,9 +99,9 @@ public class LoansController {
             @Pattern(regexp="(^$|[0-9]{10})",message = "Mobile number must be 10 digits")
             @RequestParam String mobileNumber
     ) {
-        logger.debug("Invoked method fetchLoanDetails()");
-        logger.debug("bank-correlation-id found: {}", correlationId);
+        logger.debug("Start method fetchLoanDetails()");
         LoansDto loansDto = loansService.fetchLoan(mobileNumber);
+        logger.debug("End method fetchLoanDetails()");
         return ResponseEntity.status(HttpStatus.OK).body(loansDto);
     }
 
