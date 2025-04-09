22457ea88b7dd204a1aaa3ab0297165b2d7ceee3
Yaroslav Oliinyk <niochronos@gmail.com>
2025-04-09 19:16:06 +0300

    Message Microservice: - Implemented messaging using Spring Cloud Functions

---
+++ message/src/main/java/com/oliinyk/yaroslav/message/dto/AccountsMsgDto.java
@@ -0,0 +1,16 @@
+package com.oliinyk.yaroslav.message.dto;
+
+/**
+ *
+ * @param accountNumber
+ * @param name
+ * @param email
+ * @param mobileNumber
+ */
+public record AccountsMsgDto(
+        Long accountNumber,
+        String name,
+        String email,
+        String mobileNumber
+) {
+}
