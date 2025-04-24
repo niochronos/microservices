427c9621a6ea63aacd07bb1af42d509332efab87
Yaroslav Oliinyk <niochronos@gmail.com>
2025-04-24 12:05:35 +0300

    Microservices-BOM: - init

---
+++ microservices-bom/src/main/java/com/oliinyk/yaroslav/microservices_bom/MicroservicesBomApplication.java
@@ -0,0 +1,13 @@
+package com.oliinyk.yaroslav.microservices_bom;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MicroservicesBomApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MicroservicesBomApplication.class, args);
+	}
+
+}
---
+++ microservices-bom/src/test/java/com/oliinyk/yaroslav/microservices_bom/MicroservicesBomApplicationTests.java
@@ -0,0 +1,13 @@
+package com.oliinyk.yaroslav.microservices_bom;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MicroservicesBomApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
