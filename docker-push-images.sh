4954d5b8331b0d73f9a9078e658f28dae779408f
Yaroslav Oliinyk <niochronos@gmail.com>
2025-04-29 21:00:40 +0300

    Microservices: - Updated docker push images script to tag='s20'

--- docker-push-images.sh
+++ docker-push-images.sh
@@ -11,14 +11,14 @@ declare -a images=(
     "message"
 )
 
-declare tag=":s14"
+declare tag="s20"
 
 # Loop through each images and run the commands
 for image in "${images[@]}"; do
     echo " "
-    echo "[ Docker image push docker.io/niochronos/$image$tag ]"
-    docker image push docker.io/niochronos/"$image$tag" || { echo "Docker push failed $image$tag"; exit 1; }
-    echo "[ Successfully pushed $image$tag ]"
+    echo "[ Docker image push docker.io/niochronos/$image:$tag ]"
+    docker image push docker.io/niochronos/"$image:$tag" || { echo "Docker push failed $image:$tag"; exit 1; }
+    echo "[ Successfully pushed $image:$tag ]"
 done
 
 echo " "
